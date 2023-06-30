e4b76852849484a8b8b94a5e2bf7e098683f7360
ekram <ekraom@126.com>
2023-06-30 23:51:17 +0800

    上传OpenGL03 材质

---
+++ OpenGL03/OpenGL01/Color.fs
@@ -0,0 +1,48 @@
+#version 330 core
+
+in vec3 Normal;
+in vec3 FragPos;
+
+out vec4 FragColor;
+
+struct Material
+{
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+	float shininess;
+};
+
+struct Light
+{
+	vec3 position;
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
+uniform Material material;
+uniform Light light;
+
+uniform vec3 viewPos;
+
+void main()
+{
+	//ambient
+	vec3 ambient = material.ambient * light.ambient;
+
+	//diffuse
+	vec3 normal = normalize(Normal);
+	vec3 lightDir = normalize(light.position - FragPos);
+	float diff = max(dot(normal,lightDir),0.0);
+	vec3 diffuse = light.diffuse * (diff * material.diffuse);
+
+	//specular
+	vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 reflecDir = reflect(-lightDir,normal);
+	float spec=pow(max(dot(viewDir,reflecDir),0.0),material.shininess);
+	vec3 specular = light.specular * (spec * material.specular);
+
+	vec3 result = ambient + diffuse + specular;
+	FragColor = vec4(result,1.0);
+}
